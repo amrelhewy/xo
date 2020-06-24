90f1e966e293ac75415d527a5885f8d80e8a6932
amrelhewy <amrelhewi@gmail.com>
2020-06-25 00:30:42 +0200

    make it better

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 4000;
+let server = app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+const io = require("socket.io")(server);
+app.use(express.static(path.join(__dirname,'build')))
+app.get('*',(req,res)=>{
+res.sendFile(path.join(__dirname,'/build/index.html'));
+});
+let onlinePlayers=[]
+let roomcount=1;
+let searchingPlayers=[]
+io.on("connection", (socket) => {
+  onlinePlayers.push(socket.id);
+  console.log(`new user connected to XO`);
+socket.on('searching',(data)=>{
+searchingPlayers.push({socketid:socket.id,username:data.username});
+if(searchingPlayers.length%2!=0){
+socket.join(`room${roomcount}`);
+}
+else{
+socket.join(`room${roomcount}`);
+io.sockets.in(`room${roomcount}`).emit('game_started')
+io.in(`room${roomcount}`).clients((error, clients) => {
+  if (error) throw error;
+  io.sockets.connected[clients[0]].on('ingame',()=>{
+    io.sockets.connected[clients[0]].emit('playerX');
+    io.sockets.connected[clients[0]].emit('not_your_turn');
+  })
+  io.sockets.connected[clients[1]].on('ingame',()=>{
+    io.sockets.connected[clients[1]].emit('playerO');
+  })
+  io.sockets.connected[clients[0]].on('turnover',(data)=>{
+    
+    io.sockets.in(`room${roomcount}`).emit('playdone',data)
+    
+  });
+  io.sockets.connected[clients[1]].on('turnover',(data)=>{
+ 
+    io.sockets.in(`room${roomcount}`).emit('playdone',data)
+
+    
+  });
+  io.sockets.connected[clients[0]].on('turn_finish',(data)=>{
+    io.sockets.connected[clients[1]].emit('your_turn');
+  });
+  io.sockets.connected[clients[1]].on('turn_finish',(data)=>{
+    io.sockets.connected[clients[0]].emit('your_turn');
+  });
+  io.sockets.connected[clients[1]].on('winnerX',()=>{
+    io.sockets.in(`room${roomcount}`).emit('xWon')
+  })
+  io.sockets.connected[clients[1]].on('winnerO',()=>{
+    io.sockets.in(`room${roomcount}`).emit('OWon')
+  })
+});
+
+roomcount++;
+
+}
+})
+  socket.on("disconnect", () => {
+  onlinePlayers= onlinePlayers.filter((player)=>player!=socket.id)
+  searchingPlayers=searchingPlayers.filter((p)=>socket.id!=p.id);
+    console.log("user has disconnected");
+  });
+});
---
+++ build/precache-manifest.3c89704199cd0069924d617bc362d022.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2bf7fb9c3e14b3888a5094168ae41d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7b92b3f058f491712da",
+    "url": "/static/css/main.c62294d6.chunk.css"
+  },
+  {
+    "revision": "bbea53f01c28d5c423b9",
+    "url": "/static/js/2.8c0d5d81.chunk.js"
+  },
+  {
+    "revision": "3be25bf51018a491ef7f73dfd2dd74c4",
+    "url": "/static/js/2.8c0d5d81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7b92b3f058f491712da",
+    "url": "/static/js/main.76da74e8.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "6ce8414b8c5ddcb3a60f004b213faea8",
+    "url": "/static/media/xo.6ce8414b.jpg"
+  }
+]);
