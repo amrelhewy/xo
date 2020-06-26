78488dc58193adc62c9ecbda9d791da4dcf545c2
amrelhewy <amrelhewi@gmail.com>
2020-06-26 15:59:02 +0200

    idk

--- app.js
+++ app.js
@@ -11,19 +11,40 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/build/index.html"));
 });
 let onlinePlayers = [];
-let roomcount = 1;
+let roomcount = -1;
+var rooms = [];
 let searchingPlayers = [];
 io.on("connection", (socket) => {
   onlinePlayers.push(socket.id);
   console.log(`new user connected to XO`);
-  socket.on("searching", (data) => {
-    searchingPlayers.push({ socketid: socket.id, username: data.username });
+  socket.on("searching", (lol) => {
+    searchingPlayers.push({ socketid: socket.id, username: lol.username });
     if (searchingPlayers.length % 2 != 0) {
-      socket.join(`room${roomcount}`);
+      socket.join(lol.username);
     } else {
-      socket.join(`room${roomcount}`);
-      io.sockets.in(`room${roomcount}`).emit("game_started");
-      io.in(`room${roomcount}`).clients((error, clients) => {
+      console.log(
+        Object.keys(io.sockets.adapter.rooms)[
+          Object.keys(io.sockets.adapter.rooms).length - 1
+        ]
+      );
+      socket.join(
+        Object.keys(io.sockets.adapter.rooms)[
+          Object.keys(io.sockets.adapter.rooms).length - 1
+        ]
+      );
+      io.sockets
+        .in(
+          Object.keys(io.sockets.adapter.rooms)[
+            Object.keys(io.sockets.adapter.rooms).length - 1
+          ]
+        )
+        .emit("game_started");
+      io.in(
+        Object.keys(io.sockets.adapter.rooms)[
+          Object.keys(io.sockets.adapter.rooms).length - 1
+        ]
+      ).clients((error, clients) => {
+        console.log(clients)
         if (error) throw error;
         io.sockets.connected[clients[0]].on("ingame", () => {
           io.sockets.connected[clients[0]].emit("playerX");
@@ -33,10 +54,22 @@ io.on("connection", (socket) => {
           io.sockets.connected[clients[1]].emit("playerO");
         });
         io.sockets.connected[clients[0]].on("turnover", (data) => {
-          io.sockets.in(`room${roomcount}`).emit("playdone", data);
+          io.sockets
+            .in(
+              Object.keys(io.sockets.adapter.rooms)[
+                Object.keys(io.sockets.adapter.rooms).length - 1
+              ]
+            )
+            .emit("playdone", data);
         });
         io.sockets.connected[clients[1]].on("turnover", (data) => {
-          io.sockets.in(`room${roomcount}`).emit("playdone", data);
+          io.sockets
+            .in(
+              Object.keys(io.sockets.adapter.rooms)[
+                Object.keys(io.sockets.adapter.rooms).length - 1
+              ]
+            )
+            .emit("playdone", data);
         });
         io.sockets.connected[clients[0]].on("turn_finish", (data) => {
           io.sockets.connected[clients[1]].emit("your_turn");
@@ -45,17 +78,33 @@ io.on("connection", (socket) => {
           io.sockets.connected[clients[0]].emit("your_turn");
         });
         io.sockets.connected[clients[1]].on("winnerX", () => {
-          io.sockets.in(`room${roomcount}`).emit("xWon");
+          io.sockets
+            .in(
+              Object.keys(io.sockets.adapter.rooms)[
+                Object.keys(io.sockets.adapter.rooms).length - 1
+              ]
+            )
+            .emit("xWon");
         });
         io.sockets.connected[clients[1]].on("winnerO", () => {
-          io.sockets.in(`room${roomcount}`).emit("OWon");
+          io.sockets
+            .in(
+              Object.keys(io.sockets.adapter.rooms)[
+                Object.keys(io.sockets.adapter.rooms).length - 1
+              ]
+            )
+            .emit("OWon");
         });
         io.sockets.connected[clients[1]].on("draw", () => {
-          io.sockets.in(`room${roomcount}`).emit("drw");
+          io.sockets
+            .in(
+              Object.keys(io.sockets.adapter.rooms)[
+                Object.keys(io.sockets.adapter.rooms).length - 1
+              ]
+            )
+            .emit("drw");
         });
       });
-
-      // roomcount++;
     }
   });
   socket.on("disconnect", () => {
